ff12eecd1dd4b75e6d886074516095fb9158f9b1
Aman Sharma <119524053+23Amansharma@users.noreply.github.com>
2026-02-04 23:44:42 +0530

    Time: 5 ms (83.65%), Space: 44.3 MB (52.44%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
@@ -2,19 +2,19 @@ import java.util.Arrays;
 
 class Solution {
     public boolean canMakeArithmeticProgression(int[] arr) {
-        //Array ko sort kara
+        //  Sort array
         Arrays.sort(arr);
         
-        //  Pehle do elements ka difference nikaala
+        //  Calculate-initial difference
         int diff = arr[1] - arr[0];
         
-        // Check kara ki baaki saare elements mein bhi wahi difference hai
+        // Verify-difference for all consecutive pairs
         for (int i = 2; i < arr.length; i++) {
             if (arr[i] - arr[i - 1] != diff) {
-                return false; // Agar gap alag mila toh false
+                return false;
             }
         }
         
-        return true; // Sab gap same hain toh true
+        return true;
     }
 }
