14c822d181444217d0ebb863c9b9019840c9038f
Aman Sharma <119524053+23Amansharma@users.noreply.github.com>
2026-03-16 22:29:23 +0530

    Time: 147 ms (17.65%), Space: 49.6 MB (8.82%) - LeetHub

---
+++ 1878-get-biggest-three-rhombus-sums-in-a-grid/1878-get-biggest-three-rhombus-sums-in-a-grid.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Solution {
+    public int[] getBiggestThree(int[][] grid) {
+
+        int m = grid.length;
+        int n = grid[0].length;
+
+        TreeSet<Integer> set = new TreeSet<>(Collections.reverseOrder());
+
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+
+                set.add(grid[r][c]);
+
+                for (int k = 1; r-k >= 0 && r+k < m && c-k >= 0 && c+k < n; k++) {
+
+                    int sum = 0;
+
+                    int x = r-k, y = c;
+                    for (int i = 0; i < k; i++) sum += grid[x+i][y+i];
+
+                    x = r; y = c+k;
+                    for (int i = 0; i < k; i++) sum += grid[x+i][y-i];
+
+                    x = r+k; y = c;
+                    for (int i = 0; i < k; i++) sum += grid[x-i][y-i];
+
+                    x = r; y = c-k;
+                    for (int i = 0; i < k; i++) sum += grid[x-i][y+i];
+
+                    set.add(sum);
+                }
+            }
+        }
+
+        int size = Math.min(3, set.size());
+        int[] res = new int[size];
+
+        int i = 0;
+        for (int val : set) {
+            if (i == size) break;
+            res[i++] = val;
+        }
+
+        return res;
+    }
+}
