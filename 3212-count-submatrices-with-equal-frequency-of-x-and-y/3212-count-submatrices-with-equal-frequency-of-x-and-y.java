70a88dbc7791bacf581e6bc489e08d2b44964a80
Aman Sharma <119524053+23Amansharma@users.noreply.github.com>
2026-03-19 23:11:50 +0530

    Time: 29 ms (40.38%), Space: 249.8 MB (48.08%) - LeetHub

---
+++ 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int numberOfSubmatrices(char[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        // m+1, n+1 used to avoid extra out of bound if-else conditions (1-based indexing)
+        int sum[][] = new int[m+1][n+1]; 
+        int count[][] = new int[m+1][n+1]; 
+        int find = 0;
+        
+        // FIX 1: Changed i < m-1 to i < m AND j < n-1 to j < n
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                int value = 0; 
+                int isX = 0;
+                
+                // Check condition
+                if(grid[i][j] == 'X'){
+                    value = 1;
+                    isX = 1;
+                } else if (grid[i][j] == 'Y') {
+                    value = -1;
+                }
+
+                // Apply 2D prefix sum formula
+                sum[i+1][j+1]  = value + sum[i][j+1] + sum[i+1][j] - sum[i][j];
+                count[i+1][j+1] = isX + count[i][j+1] + count[i+1][j] - count[i][j];
+                
+                // FIX 2: Changed & to && for Logical AND
+                if(sum[i+1][j+1] == 0 && count[i+1][j+1] > 0){
+                    find++;
+                }
+            }
+        }
+        return find;
+    }
+}
